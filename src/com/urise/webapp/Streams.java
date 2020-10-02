ba5e06ec8d1d44c5917b52bfb6ea4b7a747f52c1
Dimitri Marquart <dmarkvart@gmail.com>
2020-10-03 00:44:37 +0200

    implement the method via stream

--- src/com/urise/webapp/Streams.java
+++ src/com/urise/webapp/Streams.java
@@ -1,44 +1,54 @@
 package com.urise.webapp;
 
 
-import java.util.Arrays;
-import java.util.IntSummaryStatistics;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Streams {
     public static void main(String[] args) {
         int[] arr = {1, 2, 1, 4, 3, 2, 1, 2, 1, 3, 2, 1};
-        minValue(arr);
+        System.out.println(minValue(arr));
 
         List<Integer> list = Arrays.asList(1, 2, 1, 4, 3, 2, 1, 2, 1, 3, 2, 2);
-        oddOrEven(list);
+        System.out.println(oddOrEven(list));
 
 //        List<Integer> list = Arrays.asList(1, 2, 3, 4, 3, 2, 1, 5, 6, 3, 7, 5);
 //        list.stream()
 //                .distinct()
 //                .forEach(x -> System.out.print(x));
-    }
 
-    private static void minValue(int[] values) {
-        IntStream.of(values)
-                .distinct()
-                .sorted()
-                .forEach(System.out::print);
-        System.out.println();
-    }
+//       int[] values(){
+//            IntStream.of(values)
+//                    .distinct()
+//                    .sorted()
+//                    .forEach(System.out::print);
+//            return 0;
+//        }
 
-    private static void oddOrEven(List<Integer> integers) {
-        Optional<Integer> sum1 = integers.stream().reduce((acc, x) -> acc + x);
-        System.out.println(sum1);
+//        int sum = Arrays.stream(values)
+//                .reduce((left, right) -> left + right).getAsInt();
+//
+//        List<Integer> oddOrEven (List < Integer > integers) {
+//            Optional<Integer> sum1 = integers.stream().reduce((acc, x) -> acc + x)
+//        }
+
+//        Optional  <Integer> sum1 = integers.stream().reduce((acc, x) -> acc + x);
 //        integers.stream().filter(x -> (x % 2) == (sum1 % 2)).distinct().collect(Collectors.toList());
 
-        long sum2 = integers.stream().reduce(0, (u, v) -> u + v);
-        System.out.println(integers.stream().filter(x -> (x % 2)==(sum2 % 2)).distinct().collect(Collectors.toList()));
+//        long sum2 = integers.stream().reduce(0, (u, v) -> u + v);
+//        System.out.println(integers.stream().filter(x -> (x % 2)==(sum2 % 2)).distinct().collect(Collectors.toList()));
+    }
+
+    static List<Integer> oddOrEven(List<Integer> integers){
+        int sum = integers.stream().reduce((acc, x) -> acc + x).get();
+        return integers.stream().filter(x -> (x % 2) == (sum % 2)).distinct().collect(Collectors.toList());
 
     }
 
+    static int minValue(int[] values) {
+        return IntStream.of(values).distinct().sorted().reduce((i, i1) -> i+i1).getAsInt();
+    }
 }
+
